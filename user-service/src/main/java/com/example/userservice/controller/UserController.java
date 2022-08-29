024c4f7015cd74843213dfaba8557674b0de02b1
newchk610@gmail.com <gprms@1208>
2022-08-29 13:13:19 +0900

    [Feature] User-service Setting

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,27 +1,31 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.vo.Greeting;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
 public class UserController {
 
     private final Environment env;
+    private final Greeting greeting;
 
-    public UserController(Environment env) {
+    public UserController(Environment env, Greeting greeting) {
         this.env = env;
+        this.greeting = greeting;
     }
 
-    @GetMapping("/heath-check")
+    @GetMapping("/health-check")
     public String status(){
         return "It's Working in user Service.";
     }
 
     @GetMapping("/welcome")
     public String welcome(){
-        return env.getProperty("greeting.message");
+//        return env.getProperty("greeting.message");
+        return greeting.getHello();
     }
 }
