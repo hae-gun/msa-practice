7d9c64a54a4575548070754a1e9e15fae16995bf
Hae-gun <newchk610@gmail.com>
2022-09-01 22:13:54 +0900

    [Feature] order-service 추가

---
+++ demo/catalog/catalog-service/src/main/java/com/example/catalogservice/controller/CatalogController.java
@@ -0,0 +1,44 @@
+package com.example.catalogservice.controller;
+
+import com.example.catalogservice.dto.ResponseCatalog;
+import com.example.catalogservice.jpa.CatalogEntity;
+import com.example.catalogservice.service.CatalogService;
+import org.modelmapper.ModelMapper;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog-service")
+public class CatalogController {
+
+    private final CatalogService service;
+    private final Environment env;
+
+    public CatalogController(CatalogService service, Environment env) {
+        this.service = service;
+        this.env = env;
+    }
+
+    @GetMapping("/health-check")
+    public String status() {
+        return String.format("It's Working in Catalog Service on PORT %s", env.getProperty("local.server.port"));
+    }
+
+    @GetMapping("/catalogs")
+    public ResponseEntity<List<ResponseCatalog>> getAll(){
+        Iterable<CatalogEntity> catalogList = service.getAllCatalogs();
+
+        List<ResponseCatalog> result = new ArrayList<>();
+
+        catalogList.forEach(v -> result.add(new ModelMapper().map(v, ResponseCatalog.class)));
+
+        return ResponseEntity.ok(result);
+    }
+}
---
+++ demo/catalog/catalog-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+values('CATALOG-001', 'Berlin1', 100, 1500);
+insert into catalog(product_id, product_name, stock, unit_price)
+values('CATALOG-002', 'Berlin2', 120, 1500);
+insert into catalog(product_id, product_name, stock, unit_price)
+values('CATALOG-003', 'Berlin3', 150, 1500);
