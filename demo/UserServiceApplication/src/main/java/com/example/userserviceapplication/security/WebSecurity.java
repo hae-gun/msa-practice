12d821c5b6fe0323566daf04563d90c33b24f6e3
Hae-gun <newchk610@gmail.com>
2022-09-06 21:01:22 +0900

    [Feature] user-service Security 설정

--- demo/UserServiceApplication/src/main/java/com/example/userserviceapplication/security/WebSecurity.java
+++ demo/UserServiceApplication/src/main/java/com/example/userserviceapplication/security/WebSecurity.java
@@ -1,30 +1,76 @@
 package com.example.userserviceapplication.security;
 
+import com.example.userserviceapplication.service.UserService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cloud.context.environment.EnvironmentManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.servlet.Filter;
+
 @EnableWebSecurity
-@Configuration(proxyBeanMethods = false)
-@ConditionalOnDefaultWebSecurity
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-public class WebSecurity{
-    @Bean
-    @Order(SecurityProperties.BASIC_AUTH_ORDER)
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-                .authorizeRequests().antMatchers("/users/**").permitAll()
+@Configuration
+//@Configuration(proxyBeanMethods = false)
+//@ConditionalOnClass(AuthenticationManager.class)
+//@ConditionalOnDefaultWebSecurity
+//@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class WebSecurity extends WebSecurityConfigurerAdapter{
+//    @Bean
+//    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.addFilter(getAuthenticationFilter(http.getSharedObject(AuthenticationManager.class)));
+//        return http.csrf().disable()
+//                .authorizeRequests()
+////                .antMatchers("/users/**").permitAll()
+//                .antMatchers("/users/**")
+//                .hasIpAddress("192.168.0.238")
+//                .and()
+//                .headers().frameOptions().disable()
+//                .and()
+//                .build();
+//    }
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Environment env;
+
+    public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder, Environment env) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.env = env;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("192.168.0.238")
                 .and()
-                .headers().frameOptions().disable()
-                .and()
-                .build();
+                .addFilter(getAuthenticationFilter());
+        http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+        return authenticationFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
--- demo/UserServiceApplication/src/main/java/com/example/userserviceapplication/service/UserService.java
+++ demo/UserServiceApplication/src/main/java/com/example/userserviceapplication/service/UserService.java
@@ -2,8 +2,9 @@ package com.example.userserviceapplication.service;
 
 import com.example.userserviceapplication.dto.UserDto;
 import com.example.userserviceapplication.repository.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto userDto);
     UserDto getUserById(String userId);
     Iterable<UserEntity> getUserByAll();
